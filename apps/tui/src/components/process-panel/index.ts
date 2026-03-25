a6946f8b6632757b948723fd71f9b45bf2176b3d
podjamz <podjamz@gmail.com>
2026-03-25 06:25:00 +0000

    feat: debounce and throttle utilities

--- apps/tui/src/components/process-panel/index.ts
+++
@@ -1,4 +0,0 @@
-export { ProcessSidebar } from "./ProcessSidebar.js";
-export { ProcessDetailView } from "./ProcessDetailView.js";
-export { ProcessListItem } from "./ProcessListItem.js";
-export { ProcessBadge } from "./ProcessBadge.js";
